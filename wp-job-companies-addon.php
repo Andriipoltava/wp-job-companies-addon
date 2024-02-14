0713dd71621abaea48dd07ec8b399c0fcbc59d28
«AndriiPoltava» <Omelianenko993@gmail.com>
2024-02-14 11:03:40 +0200

    first commit

---
+++ wp-job-companies-addon.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: WP Job - Company Addon
+ * Plugin URI:  https://github.com/astoundify/wp-job-manager-companies
+ * Description: Output a list of all companies that have posted a job, with a link to a company profile.
+ * Author:      Black Digital
+ * Author URI:  https://blackdigitalgroup.com/
+ * Version:     0.1
+ * Text Domain: wp-job-companies-addon
+ */
